748654e44228036582d66bd14cbbd739206ab330
Riccardo Cecere <riccardo.cecere10@gmail.com>
2021-05-10 15:18:20 +0200

    Create parallel_test_run.py
    add(parallel)

---
+++ parallel_test_run.py
@@ -0,0 +1,13 @@
+from subprocess import Popen
+
+
+processes = []
+
+for counter in range(1):
+    chrome_cmd = 'export BROWSER=chrome && python test.py'
+    firefox_cmd = 'export BROWSER=firefox && python test.py'
+    processes.append(Popen(chrome_cmd, shell=True))
+    processes.append(Popen(firefox_cmd, shell=True))
+
+for counter in range(1):
+    processes[counter].wait()
