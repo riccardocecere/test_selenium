a7236394e96d8438bddc55153fbc610906d7f51e
Riccardo Cecere <riccardo.cecere10@gmail.com>
2021-05-17 16:23:19 +0200

    Update parallel_test_run.py

--- parallel_test_run.py
+++ parallel_test_run.py
@@ -9,5 +9,5 @@ for counter in range(1):
     processes.append(Popen(chrome_cmd, shell=True))
     processes.append(Popen(firefox_cmd, shell=True))
 
-for counter in range(len(processes)):
+for counter in range(len(processes)+1):
     processes[counter].wait()
