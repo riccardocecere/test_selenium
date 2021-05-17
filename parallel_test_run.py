82f05c6a354e90ecc283a290338b7782924b6afc
Riccardo Cecere <riccardo.cecere10@gmail.com>
2021-05-17 18:07:06 +0200

    Update parallel_test_run.py

--- parallel_test_run.py
+++ parallel_test_run.py
@@ -4,8 +4,8 @@ from subprocess import Popen
 processes = []
 
 for counter in range(1):
-    chrome_cmd = 'BROWSER=chrome && python test.py'
-    firefox_cmd = 'BROWSER=firefox && python test.py'
+    chrome_cmd = 'export BROWSER="chrome" && python test.py'
+    firefox_cmd = 'export BROWSER="firefox" && python test.py'
     processes.append(Popen(chrome_cmd, shell=True))
     processes.append(Popen(firefox_cmd, shell=True))
 
