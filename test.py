b0ea67b6779c0c31959008e13530934a1a3874e8
riccardocecere <riccardo.cecere10@gmail.com>
2021-05-18 15:14:40 +0200

    update(index args)

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 class HackerNewsSearchTest(unittest.TestCase):
 
     def setUp(self):
-        caps = {'browserName': sys.argv[len(sys.argv)-1]}
+        caps = {'browserName': sys.argv[len(sys.argv)]}
         address = os.getenv('NODE_HUB_ADDRESS')
         self.browser = webdriver.Remote(
             command_executor=f'http://{address}:4444/wd/hub',
