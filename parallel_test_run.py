68ae2b6a4675464618ea63d716d4872ccb836287
riccardocecere <riccardo.cecere10@gmail.com>
2021-05-18 14:57:56 +0200

    update(arguments passing)

--- parallel_test_run.py
+++ parallel_test_run.py
@@ -6,10 +6,10 @@ processes = []
 for counter in range(1):
     #chrome_cmd = 'export BROWSER=chrome && python test.py'
     #firefox_cmd = 'export BROWSER=firefox && python test.py'
-    chrome_cmd = 'python test.py chrome'
-    firefox_cmd = 'python test.py firefox'
-    processes.append(Popen(chrome_cmd, shell=True))
-    processes.append(Popen(firefox_cmd, shell=True))
+    cmd = 'python -m unittest test.py'
+    cmd = 'python -m unittest test.py'
+    processes.append(Popen(cmd, args=['chrome'], shell=True))
+    processes.append(Popen(cmd, arges=['firefox'],  shell=True))
     
     #cmd = '(export BROWSER=firefox && python -m unittest test.py) & (export BROWSER=chrome && python -m unittest test.py)'
     #processes.append(Popen(cmd, shell=True))
