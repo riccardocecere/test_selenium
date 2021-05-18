830888622566e8ba1882682d839a644de5ffc154
riccardocecere <riccardo.cecere10@gmail.com>
2021-05-18 17:03:23 +0200

    repristinated commented tests

--- test.py
+++ test.py
@@ -10,21 +10,28 @@ from selenium.webdriver.common.keys import Keys
 class HackerNewsSearchTest(unittest.TestCase):
 
     def setUp(self):
-        caps = {'browserName': os.getenv('BROWSER')}
+        browser = os.getenv('BROWSER')
+        caps = {'browserName': browser}
         address = os.getenv('NODE_HUB_ADDRESS')
-        self.browser = webdriver.Remote(
-            command_executor=f'http://{address}:4444/wd/hub',
-            desired_capabilities=caps
-        )
+        if (browser == "chrome"):
+            self.browser = webdriver.Remote(
+                command_executor=f'http://{address}:4444/wd/hub',
+                desired_capabilities=caps
+            )
+        elif (browser == 'firefox'):
+            self.browser = webdriver.Remote(
+                command_executor=f'http://{address}:4444/wd/hub',
+                desired_capabilities=caps
+            )
 
-    #def test_hackernews_search_for_testdrivenio(self):
-    #    browser = self.browser
-    #    browser.get('https://news.ycombinator.com')
-    #    search_box = browser.find_element_by_name('q')
-    #    search_box.send_keys('testdriven.io')
-    #    search_box.send_keys(Keys.RETURN)
-    #    time.sleep(3)  # simulate long running test
-    #    self.assertIn('testdriven.io', browser.page_source)
+    def test_hackernews_search_for_testdrivenio(self):
+       browser = self.browser
+       browser.get('https://news.ycombinator.com')
+       search_box = browser.find_element_by_name('q')
+       search_box.send_keys('testdriven.io')
+       search_box.send_keys(Keys.RETURN)
+       time.sleep(3)  # simulate long running test
+       self.assertIn('testdriven.io', browser.page_source)
 
     def test_hackernews_search_for_selenium(self):
         browser = self.browser
@@ -44,14 +51,14 @@ class HackerNewsSearchTest(unittest.TestCase):
         time.sleep(3)  # simulate long running test
         self.assertIn('testdriven', browser.page_source)
 
-    #def test_hackernews_search_with_no_results(self):
-    #    browser = self.browser
-    #    browser.get('https://news.ycombinator.com')
-    #    search_box = browser.find_element_by_name('q')
-    #    search_box.send_keys('?*^^%')
-    #    search_box.send_keys(Keys.RETURN)
-    #    time.sleep(3)  # simulate long running test
-    #    self.assertNotIn('<em>', browser.page_source)
+    def test_hackernews_search_with_no_results(self):
+       browser = self.browser
+       browser.get('https://news.ycombinator.com')
+       search_box = browser.find_element_by_name('q')
+       search_box.send_keys('?*^^%')
+       search_box.send_keys(Keys.RETURN)
+       time.sleep(3)  # simulate long running test
+       self.assertNotIn('<em>', browser.page_source)
 
     def tearDown(self):
         self.browser.quit()  # quit vs close?
