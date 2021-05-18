32952a467b839784d517474359ac5638b16d4459
Riccardo Cecere <riccardo.cecere10@gmail.com>
2021-05-18 12:08:41 +0200

    update(parallel execution)

--- parallel_test_run.py
+++ parallel_test_run.py
@@ -4,10 +4,13 @@ from subprocess import Popen
 processes = []
 
 for counter in range(1):
-    chrome_cmd = 'export BROWSER=chrome && python test.py'
-    firefox_cmd = 'export BROWSER=firefox && python test.py'
-    processes.append(Popen(chrome_cmd, shell=True))
-    processes.append(Popen(firefox_cmd, shell=True))
+    #chrome_cmd = 'export BROWSER=chrome && python test.py'
+    #firefox_cmd = 'export BROWSER=firefox && python test.py'
+    #processes.append(Popen(chrome_cmd, shell=True))
+    #processes.append(Popen(firefox_cmd, shell=True))
+    
+    cmd = '(export BROWSER=firefox && python -m unittest test.py) & (export BROWSER=chrome && python -m unittest test.py)'
+    processes.append(Popen(cmd, shell=True))
 
 for counter in range(len(processes)+1):
     processes[counter].wait()
